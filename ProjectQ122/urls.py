1ae76ac73b75bf988339b2fbb9ec5233131e2d66
Nock <Nockbury@gmail.com>
2021-12-13 10:46:30 +0100

    Ajout HTML base

--- ProjectQ122/urls.py
+++ ProjectQ122/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from .views import index
 
 urlpatterns = [
+    path('', index, name="page index"),
     path('admin/', admin.site.urls),
 ]
