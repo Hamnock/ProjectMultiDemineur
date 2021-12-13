b386226b4b8a5ebc2ba96e0c8baa4dfb6fc400d0
Nock <Nockbury@gmail.com>
2021-12-13 16:22:43 +0100

    modification pour ajout bd

--- ProjectQ122/views.py
+++ ProjectQ122/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 from datetime import datetime
 
 def index(request):
-    return render(request, "index.html", context={"prenon": "patrick","date" : datetime.today()})
+     return render(request, "index.html", context={"prenon": "YNname","date" : datetime.today()})
+
+def name(request):
+    if request.method == "POST":
+        return print("YNname")
--- demineur/models.py
+++ demineur/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class demineur(models.Model):
     Speudo = models.CharField(max_length=128)
     Date = models.DateField(auto_now=True)
-    Score = models.IntegerField
+    Score = models.IntegerField(default=0)
     Timer = models.FloatField(default=0.0)
     ScoreHautFait = models.IntegerField(default=0)
     def __str__(self):
